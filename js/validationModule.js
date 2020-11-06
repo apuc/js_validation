03d67dadf723b052fe816f140888e4a7dc65efc2
Z1chi <Dima-Savenko99@yandex.ru>
2020-11-06 17:46:33 +0300

    Частичная валидация без проверки правописания

--- js/validationModule.js
+++ js/validationModule.js
@@ -1,3 +1,52 @@
+let form = document.querySelector('form[data-validation="formValidation"]')
+let submitBtn = form.querySelector('[data-validation="btnSubmit"]')
+let email = form.querySelector('[data-validation="emailValid"]')
+let password = form.querySelector('[data-validation="password"]')
+let passwordConfirm = form.querySelector('[data-validation="passwordConfirmation"]')
+let number = form.querySelector('[data-validation="number"]')
+let fields = form.querySelectorAll('[data-field="field"]')
+
+let generateError = function (text){
+    let error = document.createElement('div');
+    error.className='error';
+    error.style.color='red';
+    error.innerHTML = text;
+    return error
+};
+
+let removeValidation = function() {
+    let errors = form.querySelectorAll('.error');
+    for(let i = 0; i < errors.length; i ++) {
+        errors[i].remove()
+    }
+};
+
+ let checkFieldsPresence = function () {
+     for (let i = 0; i < fields.length; i++){
+         if (!fields[i].value){
+             console.log('field is blank', fields[i] + 'not full');
+            let error = generateError('Поле заполненно неверно');
+             form[i].parentElement.insertBefore(error, fields[i]);
+
+         }
+     }
+ };
+
+ let checkPasswordMatch = function() {
+     if (password.value !== passwordConfirm.value) {
+         let error =  generateError('Пароли не совпадают');
+         password.parentElement.insertBefore(error, password);
+     }
+ };
+
+form.addEventListener('submit',function (event) {
+    event.preventDefault();
+    removeValidation();
+    checkFieldsPresence();
+    checkPasswordMatch();
+});
+
+/*
 let inputs = document.querySelectorAll('input[data-validation]');
 
 for (let input of inputs) {
@@ -24,4 +73,4 @@ for (let input of inputs) {
         }
 
     });
-}
+}*/
