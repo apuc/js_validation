6c464a33c6d44d677719d2c164445d20df2972da
Z1chi <Dima-Savenko99@yandex.ru>
2020-11-13 16:41:03 +0300

    Паттерны берутся из массива

--- js/validationModule.js
+++ js/validationModule.js
@@ -1,29 +1,17 @@
 class ValidationModule {
     form;
     fields;
-    number = /^\+\d{2}\(\d{3}\)\d{3}-\d{2}-\d{2}$/;
-    email = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    arr;
 
     constructor() {
         this.form = document.querySelector('form[data-validation="formValidation"]');
         this.fields = this.form.querySelectorAll(`[data-validation]`);
+        this.arr = new Map([
+            ['number', /^\+\d{2}\(\d{3}\)\d{3}-\d{2}-\d{2}$/],
+            ['emailValid', /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/],
+            ['password', /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,64}$/]          /**(?=.*[ -/:-@\[-`{-~]) паттерн для знаков */
+        ]);
     }
-
-    agreement = function (oneElement) {
-        !oneElement.checked ? this.generateError('Вы не приняли пользовательское соглашение', oneElement) : console.log("NET");
-    };
-
-    emailValid = function (oneElement) {
-        return this.email.test(oneElement.value);
-
-    };
-
-    selectVal = function (field) {
-        let selectedValue = field.options[field.selectedIndex].value;       //селект.опции.[селект.айдиОпции].значение
-        if (!selectedValue) {                                               //если выбранное значение селекта не тру, т.е. не имеет значения и возвращает фолс
-            this.generateError('Вы ничего не выбрали', field);         //выполняем функцию генерации ошибки с текстом,
-        }
-    };
 
     validation = function () {
         this.removeValidation();
@@ -32,12 +20,42 @@ class ValidationModule {
         }
     };
 
-    comparePassword = function (oneElement) {
-        let confirmPass = this.form.querySelector('[data-validation="passwordConfirmation require"]');
-        if (oneElement.value && confirmPass.value) {                          //если значение пароля и значение подтвержденного не null
-            if (oneElement.value !== confirmPass.value) {                     //если значения паролей не равны
-                this.generateError('Пароли не совпадают', oneElement);   //вызов функции с текстом
-            }
+    /** подбирает валидацию для элемента и вызывает соответствующие функции */
+    searchMethodValid = function (oneElement) {
+        let validationEl = oneElement.getAttribute('data-validation');
+        let arValidEl = validationEl.split(" ")[0];
+        let inputValue = !oneElement.value;//
+        switch (arValidEl) {
+
+            /** Если значение пустое, выводит error , если заполнено, вызывает функцию паттерна и принимает тру или фолс */
+            case("emailValid") :
+                let email = inputValue ? this.generateError('Поле не заполнено', oneElement) : this.patternValid(oneElement, arValidEl);
+                !email ? this.generateError('Неверный Email', oneElement) : console.log();
+                break;
+
+            /** Проверка на пустую строку, пвроерка по паттерну, проверка на совпадние паролей */
+            case("password") :
+                let passwordValid = inputValue ? this.generateError('Поле не заполнено', oneElement) : this.patternValid(oneElement, arValidEl);
+                !passwordValid ? this.generateError('Не соответствует форматированию', oneElement) : this.comparePassword(oneElement);
+                break;
+
+            case("passwordConfirmation") :
+                inputValue ? this.generateError('Поле не заполнено', oneElement) : console.log();
+                break;
+
+            case("country") :
+                this.selectVal(oneElement);
+                break;
+
+            /**  Проверка номера, в плане выбор стандарта по региону this.number = паттерн   */
+            case("number") :
+                let number = inputValue ? this.generateError('Поле не заполнено', oneElement) : this.patternValid(oneElement, arValidEl);
+                !number ? this.generateError('Номер введен неверно', oneElement) : console.log();
+                break;
+
+            case("agreement") :
+                this.agreement(oneElement);
+                break;
         }
     };
 
@@ -58,55 +76,41 @@ class ValidationModule {
         }
     };
 
-    numberValid = function (oneElement, region) {
-        return region.test(oneElement.value);
+    patternValid = function (oneElement, dataA) {
+        let pattern = this.arr.get(`${dataA}`);     /** передается дата первый дата атрибут */
+        return pattern.test(oneElement.value);      /** Возвращает фолс если значение не прошло валидацию паттерном */
     };
 
-    searchMethodValid = function (oneElement) {
-        let validationEl = oneElement.getAttribute('data-validation');
-        let arValidEl = validationEl.split(" ")[0];
-        let inputValue = !oneElement.value;
-
-        switch (arValidEl) {
-
-            case("emailValid") :
-                let email = inputValue ? this.generateError('Поле не заполнено', oneElement) : this.emailValid(oneElement);
-                !email ? this.generateError('Неверный Email', oneElement) : console.log();
-                break;
-
-            case("password") :
-                let passwordValid = inputValue ? this.generateError('Поле не заполнено', oneElement) : this.comparePassword(oneElement);
-                //  !passwordValid ? console.log() : console.log();
-                break;
-
-            case("passwordConfirmation") :
-                inputValue ? this.generateError('Поле не заполнено', oneElement) : console.log();
-                break;
-
-            case("country") :
-                this.selectVal(oneElement);
-                break;
-
-            /**  Проверка номера, в плане выбор стандарта по региону this.number = паттерн   */
-            case("number") :
-                let number = inputValue ? this.generateError('Поле не заполнено', oneElement) : this.numberValid(oneElement, this.number);
-                !number ? this.generateError('Номер введен неверно', oneElement) : console.log();
-                break;
-
-            case("agreement") :
-                this.agreement(oneElement);
-                break;
+    /**Сравнение паролей */
+    comparePassword = function (oneElement) {
+        let confirmPass = this.form.querySelector('[data-validation="passwordConfirmation require"]');
+        if (oneElement.value && confirmPass.value) {                          //если значение пароля и значение подтвержденного не null
+            if (oneElement.value !== confirmPass.value) {                     //если значения паролей не равны
+                this.generateError('Пароли не совпадают', oneElement);   //вызов функции с текстом
+            }
         }
+    };
 
+    /** проверка выбора селекта */
+    selectVal = function (oneElement) {
+        let selectedValue = oneElement.options[oneElement.selectedIndex].value;       //селект.опции.[селект.айдиОпции].значение
+        if (!selectedValue) {                                               //если выбранное значение селекта не тру, т.е. не имеет значения и возвращает фолс
+            this.generateError('Вы ничего не выбрали', oneElement);         //выполняем функцию генерации ошибки с текстом,
+        }
+    };
+
+    /** проверка статуса чекбокса */
+    agreement = function (oneElement) {
+        !oneElement.checked ? this.generateError('Вы не приняли пользовательское соглашение', oneElement) : console.log();
     };
 }
 
 let validator = new ValidationModule();
-let buttonForm = validator.form;
 /**Слушатель на кнопку сабмит, для вызова функций проверки*/
-buttonForm.addEventListener('submit', function (event) {
+validator.form.addEventListener('submit', function (event) {
     event.preventDefault();
     validator.validation();
 });
+
 
 /**--------------------------------------------------------------------------*/
